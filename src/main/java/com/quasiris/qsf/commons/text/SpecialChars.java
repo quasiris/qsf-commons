ec46e0ab4faa011e0d7b42b9448735df2f06b3ad
Thomas Bärwinkel <tbl@gmx.de>
2020-09-15 16:55:57 +0200

    add some special chars

--- src/main/java/com/quasiris/qsf/commons/text/SpecialChars.java
+++ src/main/java/com/quasiris/qsf/commons/text/SpecialChars.java
@@ -2,6 +2,6 @@ package com.quasiris.qsf.commons.text;
 
 public class SpecialChars {
 
-    public static final String ALL = "^„!¡\"“§¶%&/|=≠?¿`.:…,;∞-_<>[]{}()*+±'‘#–";
+    public static final String ALL = "^„”!¡\"“§¶%&/|=≠?¿`´'‘.:…,;∞-_<>[]{}()*+±#–®";
     public static final String NUMBERS = "0123456789";
 }

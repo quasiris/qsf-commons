919f46b1ee4a5957253c0bb3e396395141bf2113
Marius Kolkowski <marius.kolkowski@raystorm.place>
2021-07-12 13:23:15 +0200

    copy ObjectMapperBuilder from qsc-admin

---
+++ src/main/java/com/quasiris/qsf/commons/util/ObjectMapperBuilder.java
@@ -0,0 +1,19 @@
+package com.quasiris.qsf.commons.util;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectMapperBuilder {
+
+    private static final ObjectMapper defaultMapper;
+
+    static {
+        defaultMapper = new ObjectMapper();
+        defaultMapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+        defaultMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    public static ObjectMapper defaultMapper() {
+        return defaultMapper;
+    }
+}

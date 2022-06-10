3e63e30e4675cee7ce05cbe25f2b409194d427ab
Marius Kolkowski <marius.kolkowski@quasiris.de>
2022-06-10 14:33:33 +0200

    set default timeout for async client

--- src/main/java/com/quasiris/qsf/commons/http/HttpClientFactory.java
+++ src/main/java/com/quasiris/qsf/commons/http/HttpClientFactory.java
@@ -2,7 +2,7 @@ package com.quasiris.qsf.commons.http;
 
 public class HttpClientFactory {
     private final static HttpClient httpClient = new DefaultHttpClient();
-    private final static AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    private final static AsyncHttpClient asyncHttpClient = new AsyncHttpClient(1000L);
 
     public static HttpClient defaultClient() {
         return httpClient;

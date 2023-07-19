21925de15bbee39fcd4b49d2b81ab1d9218edb6a
tbl <thomas.baerwinkel@quasiris.de>
2023-07-20 00:28:46 +0200

    update qsf-dto version

--- src/test/java/com/quasiris/qsf/commons/text/date/SimpleDateParserTest.java
+++ src/test/java/com/quasiris/qsf/commons/text/date/SimpleDateParserTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SimpleDateParserTest {
     @Test
     public void test() {
         SimpleDateParser parser = new SimpleDateParser("2021-02-10");
+        assertNotNull(parser.getDate());
         assertEquals("2021-02-09T23:00:00Z", parser.getInstant().toString());
         assertEquals("2021-02-10T23:00:00Z", parser.getInstant().plus(1, ChronoUnit.DAYS).toString());
     }
